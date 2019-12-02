5468a01f6f5ad2aa645cb5ce0613bb43f7c147bb
Katie Williams <kawilliams8@gmail.com>
2019-12-01 18:47:17 -0700

    Pass test_7 rounding to whole number

--- data-types/ints_and_floats/ints_and_floats.rb
+++ data-types/ints_and_floats/ints_and_floats.rb
@@ -57,11 +57,10 @@ class IntsAndFloatsTests < Minitest::Test
   end
 
   def test_7
-    skip
     pi = 3.14
     # Using the variable defined above,
     # round the number to the nearest whole number
-    rounded = _________
+    rounded = pi.round
     assert_equal 3, rounded
   end
 
