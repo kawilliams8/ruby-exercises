82d2cfd3128be86914c811144832aa7ef9d33c4e
Katie Williams <kawilliams8@gmail.com>
2019-12-06 19:03:21 -0700

    Pass all wizard tests

--- mythical-creatures/lib/wizard.rb
+++ mythical-creatures/lib/wizard.rb
@@ -1,9 +1,10 @@
 class Wizard
-  attr_reader :name, :bearded
+  attr_reader :name, :bearded, :cast_counter
   def initialize(name, bearded: true)
     @name = name
     @bearded = bearded
     @rested = true
+    @cast_counter = 0
   end
 
   def bearded?
@@ -19,6 +20,10 @@ class Wizard
   end
 
   def cast
+    @cast_counter += 1
+    if @cast_counter >= 3
+      @rested = false
+    end
     'MAGIC MISSILE!'
   end
 end
--- mythical-creatures/test/wizard_test.rb
+++ mythical-creatures/test/wizard_test.rb
@@ -49,12 +49,17 @@ class WizardTest < Minitest::Test
   end
 
   def test_gets_tired_after_casting_three_spells
-    skip
     # create wizard
     # casts spell twice
     # check wizard is rested
     # casts spell
     # check wizard is not rested
+    wizard = Wizard.new("Rob", bearded: false)
+    wizard.cast
+    wizard.cast
+    assert wizard.rested?
+    wizard.cast
+    refute wizard.rested?
   end
 
 end
