0fa542c40f398a07a616d20dddace6c4f1c9de74
Katie Williams <kawilliams8@gmail.com>
2019-12-06 19:11:27 -0700

    Pass middle tests

--- mythical-creatures/lib/medusa.rb
+++ mythical-creatures/lib/medusa.rb
@@ -1,6 +1,11 @@
 class Medusa
-  attr_reader :name
+  attr_reader :name, :statues
   def initialize(name)
     @name = name
+    @statues = []
+  end
+
+  def stare(victim)
+    @statues << victim
   end
 end
