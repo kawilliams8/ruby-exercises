c1bc73f487099fda3941de41c9a3a2520be3ce1e
Katie Williams <kawilliams8@gmail.com>
2019-12-07 09:47:36 -0700

    Pass middle centaur tests

--- mythical-creatures/lib/centaur.rb
+++ mythical-creatures/lib/centaur.rb
@@ -3,5 +3,24 @@ class Centaur
   def initialize(name, breed)
     @name = name
     @breed = breed
+    @cranky_counter = 0
+  end
+
+  def shoot
+    @cranky_counter += 1
+    "Twang!!!"
+  end
+
+  def run
+    @cranky_counter += 1
+    "Clop clop clop clop!!!"
+  end
+
+  def cranky?
+    @cranky_counter > 2 ? true : false
+  end
+
+  def standing?
+    true  
   end
 end
