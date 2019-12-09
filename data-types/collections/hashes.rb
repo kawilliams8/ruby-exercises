97dff5343d499f6698bf729533a99cb0af818aff
Katie Williams <kawilliams8@gmail.com>
2019-12-09 09:54:21 -0700

    Pass test_11

--- data-types/collections/hashes.rb
+++ data-types/collections/hashes.rb
@@ -121,7 +121,6 @@ class HashesTest < Minitest::Test
   end
 
   def test_11
-    skip
     books = {
       John_Steinbeck: "Grapes of Wrath",
       Harper_Lee: "To Kill a Mockingbird"
@@ -130,7 +129,7 @@ class HashesTest < Minitest::Test
     # add a new key of :Ernest_Hemmingway
     # with a value of "For Whom the Bell Tolls"
     # in the line below
-    ______
+    books[:Ernest_Hemmingway] = "For Whom the Bell Tolls"
     expected = {
       John_Steinbeck: "Grapes of Wrath",
       Harper_Lee: "To Kill a Mockingbird",
